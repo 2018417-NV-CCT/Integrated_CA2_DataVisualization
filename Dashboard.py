06328df1e2227d04233432ccbb5f4829a4dde170
2018417-NV-CCT <2018417@student.cct.ie>
2026-05-24 20:48:09 +0100

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -3,3 +3,12 @@ import pandas as pd
 import numpy as np
 
 st.title("Women's Clothing Reviews Dashboard")
+
+@st.cache_data
+def load_data(nrows):
+    data = pd.read_csv("data_dashboard.csv", nrows=nrows)
+
+#converting features names to lowercase
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    return data
