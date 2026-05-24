021dc5ab6a8b592db3fe5f485de4bf018dcc7ca1
2018417-NV-CCT <2018417@student.cct.ie>
2026-05-24 20:04:46 +0100

    Update 2018417_CA2_Data_Visualization.py

--- 2018417_CA2_Data_Visualization.py
+++ 2018417_CA2_Data_Visualization.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
 
 
-
-st.title("Womens' Clothing")
+st.set_page_config(
+	page_title="Women's Clothing Reviews Dashboard",
+	layout="wide",
+	initial_sidebar-state="expanded",
+)
