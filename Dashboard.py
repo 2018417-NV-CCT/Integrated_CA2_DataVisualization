9aea6b56d85448ca7ecf1ff69974e15290f6f4e3
2018417-NV-CCT <2018417@student.cct.ie>
2026-05-24 21:07:23 +0100

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -9,7 +9,7 @@ def load_data(nrows):
     data = pd.read_csv("data_dashboard.csv", nrows=nrows)
 
 #dropping first feature
-    #data = data.loc[:, ~dta.columns.str.contains("^Unnamed")]
+    data = data.loc[:, ~dta.columns.str.contains("^Unnamed")]
 
 #converting features names to lowercase
     lowercase = lambda x: str(x).lower()
